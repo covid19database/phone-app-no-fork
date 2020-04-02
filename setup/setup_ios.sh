e03257ab7250c769b2d8d0cb3c2851b75f14ddd6
K. Shankari <shankari@eecs.berkeley.edu>
2020-04-01 18:10:47 -0700

    Fix the xcodeselect + actually check in setup files

---
+++ setup/setup_ios.sh
@@ -0,0 +1,19 @@
+export NVM_VERSION=0.35.3
+export NODE_VERSION=9.4.0
+
+echo "Installing the correct version of nvm"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v$NVM_VERSION/install.sh | bash
+
+echo "Setting up the variables to run nvm"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+echo "Installing the correct node version"
+nvm install $NODE_VERSION
+
+echo "Upgrade the version of npm to the correct one"
+npm install -g npm@6.0.0
+
+git remote add upstream https://github.com/covid19database/phone-app.git
+
---
+++ setup/setup_ios_native.sh
@@ -0,0 +1,27 @@
+COCOAPODS_VERSION=1.7.5
+EXPECTED_PLUGIN_COUNT=15
+
+# Setup the development environment
+source setup/setup_ios.sh
+
+./bin/configure_xml_and_json.js cordovabuild
+
+echo "Setting up all npm packages"
+npm install
+
+echo "Updating bower"
+npx bower update
+
+echo "Installing cocoapods"
+export PATH=~/.gem/ruby/2.6.0/bin:$PATH
+gem install --no-document --user-install cocoapods -v $COCOAPODS_VERSION
+pod setup
+
+npx cordova prepare
+
+INSTALLED_COUNT=`npx cordova plugin list | wc -l`
+while [ $INSTALLED_COUNT -ne 15];
+do
+    sleep 5
+    npx cordova prepare
+done
